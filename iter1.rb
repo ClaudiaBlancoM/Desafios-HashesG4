48703a087ee9e42af180943474280d93cd7649ba
ClaudiaBlancoM <cabm0626@gmail.com>
2022-04-05 19:32:59 -0400

    first commit

---
+++ iter1.rb
@@ -0,0 +1,16 @@
+ventas = {
+Enero: 15000,
+Febrero: 22000,
+Marzo: 12000,
+Abril: 17000,
+Mayo: 81000,
+Junio: 13000,
+Julio: 21000,
+Agosto: 41200,
+Septiembre: 25000,
+Octubre: 21500,
+Noviembre: 91000,
+Diciembre: 21000
+}
+# Crear el programa iter1.rb que itere el hash ventas y muestre en pantalla todas los ventas
+# superiores a 45000 (sólo el valor de la venta).
